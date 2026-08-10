1d2650ad17ddc8aaf512eb3077761851d62c84cd
Abhishek Patel <abhishekpatel.cse@charusat.ac.in>
2026-08-10 12:53:11 +0530

    folder renamed

--- ems/backend/src/utils/qrToken.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-// NFR-2: cryptographically random, unique per registration, not derived from event_id.
-function generateQrToken() {
-  return crypto.randomBytes(24).toString("hex"); // 48 hex chars
-}
-
-module.exports = { generateQrToken };
